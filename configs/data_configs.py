996aa9944d1f3980aa0dccfce3b0ea4945b60f42
“musikisomorphie” <wujiqing9@gmail.com>
2022-04-06 10:10:13 +0200

    integrate style2/3

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -3,11 +3,25 @@ from configs.paths_config import dataset_paths
 
 
 DATASETS = {
-	'ffhq_encode': {
-		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['ffhq'],
-		'train_target_root': dataset_paths['ffhq'],
-		'test_source_root': dataset_paths['celeba_test'],
-		'test_target_root': dataset_paths['celeba_test']
-	}
+    'ffhq_encode': {
+        'transforms': transforms_config.EncodeTransforms,
+        'train_source_root': dataset_paths['ffhq'],
+        'train_target_root': dataset_paths['ffhq'],
+        'test_source_root': dataset_paths['celeba_test'],
+        'test_target_root': dataset_paths['celeba_test']
+    },
+    'ham10k': {
+        'transforms': transforms_config.MedTransforms,
+        'train_source_root': dataset_paths['ham10k'],
+        'train_target_root': dataset_paths['ham10k'],
+        'test_source_root': dataset_paths['ham10k'],
+        'test_target_root': dataset_paths['ham10k'],
+    },
+    'rxrx1': {
+        'transforms': transforms_config.MedTransforms,
+        'train_source_root': dataset_paths['rxrx1'],
+        'train_target_root': dataset_paths['rxrx1'],
+        'test_source_root': dataset_paths['rxrx1'],
+        'test_target_root': dataset_paths['rxrx1'],
+    }
 }
