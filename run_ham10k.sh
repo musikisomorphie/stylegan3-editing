09602723bdae7f4f96b9878c00ea2a566c963711
“musikisomorphie” <wujiqing9@gmail.com>
2022-04-14 10:30:06 +0200

    ready for dgx

---
+++ run_ham10k.sh
@@ -0,0 +1,254 @@
+# 1000.pt
+python inversion/scripts/train_restyle_e4e.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetProgressiveBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_e4e2_10 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--use_w_pool=True \
+--w_discriminator_lambda=0.1 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--truncation_psi=0.7 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/1000.pt
+
+python inversion/scripts/train_restyle_psp.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_psp2_10 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/1000.pt
+
+# 0800.pt
+python inversion/scripts/train_restyle_e4e.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetProgressiveBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_e4e2_08 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--use_w_pool=True \
+--w_discriminator_lambda=0.1 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--truncation_psi=0.7 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0800.pt
+
+python inversion/scripts/train_restyle_psp.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_psp2_08 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0800.pt
+
+# 0600.pt
+python inversion/scripts/train_restyle_e4e.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetProgressiveBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_e4e2_06 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--use_w_pool=True \
+--w_discriminator_lambda=0.1 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--truncation_psi=0.7 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0600.pt
+
+python inversion/scripts/train_restyle_psp.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_psp2_06 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0600.pt
+
+# 0200.pt
+python inversion/scripts/train_restyle_e4e.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetProgressiveBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_e4e2_02 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--use_w_pool=True \
+--w_discriminator_lambda=0.1 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--truncation_psi=0.7 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0200.pt
+
+python inversion/scripts/train_restyle_psp.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_psp2_02 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0200.pt
+
+# 0000.pt
+python inversion/scripts/train_restyle_e4e.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetProgressiveBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_e4e2_00 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--use_w_pool=True \
+--w_discriminator_lambda=0.1 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--truncation_psi=0.7 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0000.pt
+
+python inversion/scripts/train_restyle_psp.py \
+--dataset_type=ham10k \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/ham10k_psp2_00 \
+--max_steps=100000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg=True \
+--lpips_lambda=0 \
+--l2_lambda=10 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--max_val_batches=8 \
+--stylegan_weights=Data/non_IID/encoder/ham10k_tiny2/0000.pt
